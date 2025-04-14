3ceebb102da440efb8029738d516960d4f9c3f72
Hope <hopemoore@gmail.com>
2025-04-13 20:08:31 -0400

    Updated files

---
+++ addresses.js
@@ -0,0 +1,42 @@
+// Example of one address object
+
+const Address = {
+    name: "School of Visual Communication",
+    street1: "Schoonover Center 250",
+    street2: "20 E. Union Street",
+    city: "Athens",
+    state: "OH",
+    zipCode: "45701"
+};
+
+// Example of an array with address objects - it's a good idea to use id numbers and use a for-loop that checks for the ID and gets only the info tied to that ID
+
+const AddressList = [
+    {
+        id: 1,
+        name: "School of Visual Communication",
+        street1: "Schoonover Center 250",
+        street2: "20 E. Union Street",
+        city: "Athens",
+        state: "OH",
+        zipCode: "45701"
+    },
+    {
+        id: 2,
+        name: "Scripps College of Communication",
+        street1: "Schoonover Center",
+        street2: "1 Ohio University",
+        city: "Athens",
+        state: "OH",
+        zipCode: "45701-2979"
+    },
+    {
+        id: 3,
+        name: "Kennedy Museum of Art",
+        street1: "100 Ridges Circle",
+        street2: "",
+        city: "Athens",
+        state: "OH",
+        zipCode: "45701"
+    }
+];
